0fa819d0246ac2ade9130b6112b2cb3fec25a12f
Jason Luckow <ijasonluck@gmail.com>
2021-09-03 17:29:38 -0500

    Deploying boilerplate api code

---
+++ src/Constants.js
@@ -0,0 +1,6 @@
+const Constants = {
+	//APIRoot: 'http://api.ecestockroom.dev.mpadgett.net/v1/'
+	APIRoot: 'https://s25q5af2e3.execute-api.us-east-1.amazonaws.com/Prod/'
+}
+
+export default Constants;
