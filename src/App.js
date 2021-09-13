0a0f71963ac609ad4c01df95cc9297a37ebec19d
martyfitz <44929526+martyfitz@users.noreply.github.com>
2021-09-13 09:40:31 -0500

    ETS-18 Frontend: redesign sign in

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+import Sharingan from './Sharingan.png'
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import './SignIn.css'
+import { withAuthenticator, AmplifySignIn, AmplifySignOut, AmplifySignUp, AmplifyAuthenticator, AmplifyAuthContainer } from '@aws-amplify/ui-react'
+//import {ThemeProvider, theme, ColorModeProvider, CSSReset} from '@chakra-ui/core'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+
+const AuthorizeApp = () => {
+
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+    return authState ===AuthState.SignedIn && user ?(
+
+        <div className="App">
+            <img src={Sharingan} className="App-logo" alt="logo" />
+            <h1>
+                Welcome, {user.username}
+            </h1>
+            <AmplifySignOut
+            style={{
+                buttontext: '10px 10px'
+            }}
+            />
+        </div>
+
+    ):(
+        <AmplifyAuthContainer>
+            <div className="SignIn">
+                <AmplifyAuthenticator>
+                    <AmplifySignUp
+
+                        slot="sign-up"
+                        formFields={[
+                            {
+                                type: "username",
+                                placeholder: "Enter Username"
+                            },
+                            {
+                                type: "password",
+                                placeholder: "Enter Password"
+                            },
+                            {
+                                type: "email",
+                                placeholder: "Enter Email Address"
+                            },
+                            {
+                                type: "phone_number"
+                            }
+                        ]} />
+                        <AmplifySignIn
+                            headerText="Sign in to your eye tracking account"
+                            slot="sign-in"
+                        />
+                </AmplifyAuthenticator>
+            </div>
+        </AmplifyAuthContainer>
+    );
 }
-
-export default withAuthenticator(App);
+export default AuthorizeApp;
