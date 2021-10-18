7d4dffa2e82352ba425928f67b917a83e06aa320
martyfitz <44929526+martyfitz@users.noreply.github.com>
2021-10-18 16:57:17 -0500

    ETS-26: Material UI implementation v2

--- src/Pages/Amplify/Register.js
+++ src/Pages/Amplify/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Auth } from 'aws-amplify';
 import AmplifyInput from "./AmplifyInput";
 import { Link, useHistory } from 'react-router-dom';
+import bg from '../../Images/EyeTracking.jpg'
 
 const Register = () => {
   let history = useHistory();
@@ -30,40 +31,43 @@ const Register = () => {
   }
 
   return (
-    <div className="container w-4/12 w-medium">
-      <div className="bg-white shadow-xl rounded px-12 pt-6 pb-8 mb-4">
-        <h3 className="text-lg text-gray-700">Register</h3>
-        <AmplifyInput
-          labelName='Email:'
-          value={user.username}
-          handleInputChange={(e) => handleInputChange(e, 'username')}
-        />
-        <AmplifyInput
-          labelName='Password:'
-          type="password"
-          value={user.password}
-          handleInputChange={(e) => handleInputChange(e, 'password')}
-        />
-        <div className="flex items-center justify-between">
-          <button
-            className="mt-4 mb-4 w-full sm:w-auto border border-transparent px-6 py-3 text-base font-semibold leading-snug bg-gray-900 text-white rounded-md shadow-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800 transition ease-in-out duration-150 hover:bg-gray-600"
-            type="button"
-            onClick={() => signUp()}
-          >
-            Send
-          </button>
-        </div>
-        <div className="w-full">
-          <hr />
-          <p className="text-gray-700 pb-2 pt-2 text-sm">You already have an account?</p>
-          <Link
-            to={{
-              pathname: '/log-in'
-            }}
-            className="pt-2 text-sm text-blue-500 hover:text-blue-600"
-          >
-            Long in
-          </Link>
+    <div className="bg-none">
+
+      <div className="container w-4/12 w-medium">
+        <div className="bg-white shadow-xl rounded px-12 pt-6 pb-8 mb-4">
+          <h3 className="text-lg text-gray-700">Register</h3>
+          <AmplifyInput
+            labelName='Email:'
+            value={user.username}
+            handleInputChange={(e) => handleInputChange(e, 'username')}
+          />
+          <AmplifyInput
+            labelName='Password:'
+            type="password"
+            value={user.password}
+            handleInputChange={(e) => handleInputChange(e, 'password')}
+          />
+          <div className="flex items-center justify-between">
+            <button
+              className="mt-4 mb-4 w-full sm:w-auto border border-transparent px-6 py-3 text-base font-semibold leading-snug bg-gray-900 text-white rounded-md shadow-md hover:bg-gray-800 focus:outline-none focus:bg-gray-800 transition ease-in-out duration-150 hover:bg-gray-600"
+              type="button"
+              onClick={() => signUp()}
+            >
+              Send
+            </button>
+          </div>
+          <div className="w-full">
+            <hr />
+            <p className="text-gray-700 pb-2 pt-2 text-sm">You already have an account?</p>
+            <Link
+              to={{
+                pathname: '/log-in'
+              }}
+              className="pt-2 text-sm text-blue-500 hover:text-blue-600"
+            >
+              Log in
+            </Link>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        extend:{
+            backgroundImage:{
+                'eye-tracking': "url(./Images/EyeTracking.jpg)"
+            }
+        }
+    }
+}
