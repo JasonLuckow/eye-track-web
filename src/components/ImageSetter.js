f248eb4f05042c4723804b48e51e17af00ec0c2a
martyfitz <44929526+martyfitz@users.noreply.github.com>
2021-10-25 00:59:23 -0500

    ETS-36: Built Assessment

--- src/components/ImageSetter.js
+++ src/components/ImageSetter.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {ImageList} from "@material-ui/core";
 
+
+
+
 export default function ImageSetter(props){
-    const {gap, cols, rowHeight, variant, children} = props;
+    const {gap, cols, rowHeight, variant, children, height, width} = props;
     return(
         <ImageList
-            gap={gap || 4}
             cols={cols || 4}
+            gap={gap || 4}
             rowHeight={rowHeight || 'auto'}
             variant={variant || 'standard'}
         >
