590382f99adea78242d8ca1d19430909c4749b06
Jason Luckow <ijasonluck@gmail.com>
2021-10-31 22:10:30 -0500

    changing front cam import

--- src/components/FrontCam.js
+++ src/components/FrontCam.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {useEffect} from 'react';
-import webgazer from 'webgazer';
+import webgazer from '../../node_modules/webgazer/dist/webgazer.commonjs2';
 
 export default function FrontCam() {
     useEffect(()=>{
         webgazer.setGazeListener((data, clock)=>{
-            console.log(data, clock);
+            // console.log(data, clock);
         }).begin();
     }, []);
     return null
